212e46ca7493980db6db3e92b2fb5dad0ee5bcae
mariateresepettersson <mariateresepettersson>
2024-04-14 15:38:50 +0200

    Create a factory function

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
 const statusButton = document.querySelector("button");
 const pets = document.querySelector(".all-pets");
+
+//create a factory function that can recieve 2 parameters 
+
+const createPet = function (name, species) {
+    const pet = {
+        name: name,
+        species: species,
+        isTired: 5,
+        sleep: function () {
+            console.log(`${this.name} needs a nap. Zzzz...`);
+            this.isTired = 1;
+        },
+        play: function () {
+            if (this.isTired === 10) {
+                console.log(`Too tired to play.`);
+                this.sleep();
+            } else {
+                console.log(`Yay! ${this.name} loves to play!`);
+                this.isTired += 1;
+            }
+        }
+    };
+    return pet;
+};
